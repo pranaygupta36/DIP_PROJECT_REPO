364a469f18d7047ad2008357141612f9e2fdd94e
pranaygupta36 <pranayguptastudent@gmail.com>
2018-11-01 01:17:34 +0530

    report in jupyter-notebooks

--- flashAdj.py
+++ flashAdj.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-def flashAdj(ima, imf, alpha ):
+def flashAdj(ima, imf, alpha):
     ya = cv2.cvtColor(ima, cv2.COLOR_BGR2YCR_CB)
     yf = cv2.cvtColor(imf, cv2.COLOR_BGR2YCR_CB)
-    im = (1-alpha)*ya + alpha*yf;
+    im = alpha*ya + (1-alpha)*yf
+    im = im.astype(np.uint8)
     im = cv2.cvtColor(im, cv2.COLOR_YCR_CB2RGB)
-
     return im
